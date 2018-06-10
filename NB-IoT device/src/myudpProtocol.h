92a129947f0395e51ecd9d88fa6a298ab6ca6b6d
Isaranu Janthong <isaranujanthong@yahoo.com>
2018-06-10 23:22:58 +0700

    Update myudpProtocol.h

--- NB-IoT device/src/myudpProtocol.h
+++ NB-IoT device/src/myudpProtocol.h
@@ -1,1 +1,23 @@
+#ifndef myudpProtocol_h
+#define myudpProtocol_h
+
+#include "Arduino.h"
+#include "AIS_NB_BC95.h"
+
+class myudpProtocol
+{
+
+public:
+
+  bool begin();
+  String sendDashboard(float t, float h, String rssi, String device_id, String token);
+  String readRSSI();
+
+private:
+  String _t, _h;
+  String _device_id, _token, _packet, _rssi;
+  String _response;
+};
+
+#endif
 
