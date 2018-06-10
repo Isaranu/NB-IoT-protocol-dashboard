233f5ad507c5e7c0c56b7c2561508836ef5ddd6f
Isaranu Janthong <isaranujanthong@yahoo.com>
2018-06-10 19:39:33 +0700

    Create AIS_BC_95_connect.ino

---
+++ NB-IoT device/AIS_BC_95_connect/AIS_BC_95_connect.ino
@@ -0,0 +1,51 @@
+#include "AIS_NB_BC95.h"
+
+String apnName = "devkit.nb";
+String serverIP = "8.8.8.8";  /* Testing IP server */
+String serverPort = "5683";   /* Default port UDP : 5683 */
+String udpData = "HelloWorld";
+
+AIS_NB_BC95 AISnb;
+
+const long interval = 20;  /* set delay loop second */
+unsigned long previousMillis = 0;
+
+long cnt = 0;
+void setup()
+{ 
+  AISnb.debug = true;
+  
+  Serial.begin(9600);
+ 
+  AISnb.setupDevice(serverPort);
+
+  String ip1 = AISnb.getDeviceIP();  
+  delay(1000);
+  
+  pingRESP pingR = AISnb.pingIP(serverIP);
+  previousMillis = millis();
+
+}
+void loop()
+{ 
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval*1000)
+    {
+      cnt++;
+           
+      /* Send string message to UDP protocol */
+      UDPSend udp = AISnb.sendUDPmsgStr(serverIP, serverPort, udpData);
+
+      /* Read byte length of data */
+      Serial.println("Data length : " + String(udpData.length()) + " byte.");
+
+      /* Check current RSSI level */
+      signal chkRssi = AISnb.getSignal();
+      String rssi = chkRssi.rssi;
+      Serial.println("Current RSSI level : " + String(rssi) + " dbm.");
+   
+      previousMillis = currentMillis;
+    }
+  
+    UDPReceive resp = AISnb.waitResponse();
+}
