1bd19a557d4c00cec7d277fa4a71f19964668185
Isaranu Janthong <isaranujanthong@yahoo.com>
2018-06-11 01:13:11 +0700

    Update udp.js

--- server/udp.js
+++ server/udp.js
@@ -8,7 +8,7 @@ code.isaranu.com
 
 /* Import mongojs module */
 var mongojs = require("mongojs");
-var nbiotdb = require('nbiotdb');
+var nbiotdb = mongojs('nbiotdb');
 
 /* Import Promise module */
 var Promise = require('promise');
@@ -19,7 +19,7 @@ var split = require('split-string');
 /* Setup UDP protocol connection */
 var dgram = require("dgram");
 var server = dgram.createSocket("udp4");
-var HOST = '0.0.0.0';   /* Server IP address */
+var HOST = 'x.x.x.x';   /* Server IP address */
 var PORT = 5683;       /* udp port (DEFAULT: 5683) */
 
 var remote_port, remote_address;
@@ -31,8 +31,10 @@ var diff_utc, diff_utc_sec, diff_utc_min, diff_utc_hr, sec_digit;
 /* Start listening connection from IoT */
 server.on("listening", function(){
     var address = server.address();
-    console.log("My udp protocol start listen on " + address.address + ":" + address.port);
+    console.log("My udp protocol start listen on port " + address.port);
 });
+
+server.bind(PORT);
 
 server.on("message", function (message, remote) {
 
@@ -45,29 +47,99 @@ server.on("message", function (message, remote) {
     /* 1. Split incoming message */
     var splited_arr = split(parsedMsg, {separator:':'});
 
-    var device_id = splited_arr[0];
-    var rssi = splited_arr[1];
-    var  = splited_arr[2];
-    var  = splited_arr[3];
-    var  = splited_arr[4];
-    var  = splited_arr[5];
+    var split_1 = splited_arr[0];
+    var split_2 = splited_arr[1];
+    var split_3 = splited_arr[2];
+    var split_4  = splited_arr[3];
+    var split_5  = splited_arr[4];
 
     /* Print out check */
     console.log('- Parsed message -');
-    console.log('device_id: ' + device_id);
-    console.log('rssi: ' + rssi);
-    console.log(say);
+    console.log('split_1: ' + split_1); // TEMPERATURE
+    console.log('split_2: ' + split_2); // HUMIDITY
+    console.log('split_3: ' + split_3); // RSSI
+    console.log('split_4: ' + split_4); // DEVICE ID.
+    console.log('split_5: ' + split_5); // TOKEN
 
-    record_rssi(userid, key, lat, lng, rssi, say);
+    /* Response to device */
+    /*
+    clirep = new Buffer('OK');
+    server.send(clirep, 0, clirep.length, remote_port, remote_address, function(err){
+      if(err) console.log(err);
+      console.log('response OK');
+    });
+    */
+
+    recordData(split_1, split_2, split_3, split_4, split_5);
+
 });
 
-async function record_rssi(_userid, _key, _lat, _lng, _rssi, _say){
-
-  // 2. Check status active or not, devkey existed or not
-  await chk_active_token(_userid, _key, _lat, _lng, _rssi, _say);
-
+async function recordData(_temp, _humid, _rssi, _devid, _token){
+  await recordDataToMongo(_temp, _humid, _rssi, _devid, _token);
 }
 
+function recordDataToMongo(_temp_val, _humid_val, _rssi_val, _devid_val, _token_val){
+  return new Promise(function(resolve, reject){
+
+    /* 1. Check token to permission our server */
+    if(_token_val == 'qwerty1234'){
+
+      /* permission passed */
+      /* 2. Start record data to Database */
+
+      d = new Date();
+      ts = d.getTime();
+
+      var nbiotdb_collection = nbiotdb.collection(_devid_val);
+      nbiotdb_collection.insert({
+
+        /* Dataset */
+        device_id: String(_devid_val),
+        t: Number(_temp_val),
+        h: Number(_humid_val),
+        rssi: Number(_rssi_val),
+        ts: Number(ts)
+
+      },function(err){
+        if(err){
+          /* Response to device */
+          console.log(err);
+
+          clirep = new Buffer('error - db write error');
+          server.send(clirep, 0, clirep.length, remote_port, remote_address, function(err){
+            if(err) console.log(err);
+            console.log('error - unauthenticated device');
+
+          });
+        }else {
+
+          /* Response to device */
+          clirep = new Buffer('200 OK - Record completed');
+          server.send(clirep, 0, clirep.length, remote_port, remote_address, function(err){
+            if(err) console.log(err);
+            console.log('200 OK - Record completed');
+          });
+
+        }
+      });
+
+
+    }else {
+      /* Response to device */
+      clirep = new Buffer('error - unauthenticated device');
+      server.send(clirep, 0, clirep.length, remote_port, remote_address, function(err){
+        if(err) console.log(err);
+        console.log('error - unauthenticated device');
+      });
+    }
+
+  });
+}
+
+
+
+
+/*
 function chk_active_token(_struserid, _strkey, _strlat, _strlng, _strrssi, _strsay){
   return new Promise(function(resolve, reject){
 
@@ -184,5 +256,4 @@ function chk_active_token(_struserid, _strkey, _strlat, _strlng, _strrssi, _strs
 
   });
 }
-
-server.bind(PORT);
+*/
