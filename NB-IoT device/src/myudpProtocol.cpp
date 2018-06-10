1186052ee4ea0ccd4853ddfacd35e1ff5d3cea05
Isaranu Janthong <isaranujanthong@yahoo.com>
2018-06-11 01:10:05 +0700

    Update myudpProtocol.cpp

--- NB-IoT device/src/myudpProtocol.cpp
+++ NB-IoT device/src/myudpProtocol.cpp
@@ -1,9 +1,12 @@
+
 #include "myudpProtocol.h"
 
 /* AIS_NB_BC95 library parameter */
 String apnName = "devkit.nb";
 String serverIP = "xx.xxx.xxx.xx" ; /* My Server IP address on Google Cloud */
 String serverPort = "5683";
+
+#define TIMEOUT 500
 
 AIS_NB_BC95 AISnb;
 
@@ -49,11 +52,20 @@ String myudpProtocol:: sendDashboard(float t, float h, String rssi, String devic
   UDPSend udp = AISnb.sendUDPmsgStr(serverIP, serverPort, _packet);
 
   /* Wait response from server */
-  while (_response.length() == 0) {
+  wait_resp = 0;
+
+  while (_response.length() == 0 && wait_resp < TIMEOUT) {
+    delay(30);
     UDPReceive resp = AISnb.waitResponse();
     _response = resp.data;
+    wait_resp++;
   }
 
+  if(wait_resp >= TIMEOUT){
+    Serial.println("Response Timeout.");
+  }
+
+    Serial.println("wait_resp : " + String(wait_resp));
   _response = ""; /* Clear response variable */
   return "OK";
 }
